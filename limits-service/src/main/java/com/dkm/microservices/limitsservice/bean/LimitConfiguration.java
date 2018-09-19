1f2707e8f1edcbe54bfe80e1acfb0605aa3a8d63
David Karol Maia <1209553@sarah.br>
2018-09-19 15:05:56 -0300

    setted the limits in file properties and make the configuration class

---
+++ limits-service/src/main/java/com/dkm/microservices/limitsservice/bean/LimitConfiguration.java
@@ -0,0 +1,34 @@
+package com.dkm.microservices.limitsservice.bean;
+
+public class LimitConfiguration {
+	
+	private Integer maximum;
+	private Integer minimum;
+
+	protected LimitConfiguration() {
+		
+	}
+	
+	public LimitConfiguration(Integer maximum, Integer minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+	
+	public Integer getMaximum() {
+		return maximum;
+	}
+	
+	public void setMaximum(Integer maximum) {
+		this.maximum = maximum;
+	}
+	
+	public Integer getMinimum() {
+		return minimum;
+	}
+	
+	public void setMinimum(Integer minimum) {
+		this.minimum = minimum;
+	}
+	
+}
